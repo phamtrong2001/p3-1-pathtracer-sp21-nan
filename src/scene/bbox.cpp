02bca54a605f86cd4cc61dcbf3b0c5c4d7ae988d
Eric Markley <e.markley7@verizon.net>
2021-03-16 17:09:04 -0700

    Part 2 Complete

--- src/scene/bbox.cpp
+++ src/scene/bbox.cpp
@@ -13,10 +13,43 @@ bool BBox::intersect(const Ray& r, double& t0, double& t1) const {
   // Implement ray - bounding box intersection test
   // If the ray intersected the bouding box within the range given by
   // t0, t1, update t0 and t1 with the new intersection times.
+    float mintx = (min.x - r.o.x)/r.d.x;
+    float maxtx = (max.x - r.o.x)/r.d.x;
+    if(mintx>maxtx){
+        std::swap(mintx,maxtx);
+    }
+    float minty = (min.y - r.o.y)/r.d.y;
+    float maxty = (max.y - r.o.y)/r.d.y;
+    if(minty>maxty){
+        std::swap(minty,maxty);
+    }
+    float mintz = (min.z - r.o.z)/r.d.z;
+    float maxtz = (max.z - r.o.z)/r.d.z;
+    if(mintz>maxtz){
+        std::swap(mintz,maxtz);
+    }
 
+//    if(t0>t1){
+//        std::swap(t0,t1);
+//    }
 
-  return true;
-
+    if( mintx<=maxty && minty <= maxtx){
+        float mintxy = (mintx > minty ? mintx : minty);
+        float maxtxy = (maxtx < maxty ? maxtx : maxty);
+        if( mintxy<=maxtz && mintz <= maxtxy){
+            float minbt = (mintxy > mintz ? mintxy : mintz);
+            float maxbt = (maxtxy < maxtz ? maxtxy : maxtz);
+            if( minbt<=r.max_t && r.min_t <= maxbt){
+                return true;
+            }
+        }
+        else{
+            return false;
+            }
+    }
+    else{
+        return false;
+    }
 }
 
 void BBox::draw(Color c, float alpha) const {
--- src/scene/bvh.cpp
+++ src/scene/bvh.cpp
@@ -57,18 +57,98 @@ BVHNode *BVHAccel::construct_bvh(std::vector<Primitive *>::iterator start,
   // size configuration. The starter code build a BVH aggregate with a
   // single leaf node (which is also the root) that encloses all the
   // primitives.
+    
+    struct less_than_x
+    {
+        inline bool operator() (const Primitive* prim1, const Primitive* prim2)
+        {
+            return (prim1->get_bbox().centroid().x < prim2->get_bbox().centroid().x );
+        }
+    };
+    struct less_than_y
+    {
+        inline bool operator() (const Primitive* prim1, const Primitive* prim2)
+        {
+            return (prim1->get_bbox().centroid().y < prim2->get_bbox().centroid().y );
+        }
+    };
+    struct less_than_z
+    {
+        inline bool operator() (const Primitive* prim1, const Primitive* prim2)
+        {
+            return (prim1->get_bbox().centroid().z < prim2->get_bbox().centroid().z );
+        }
+    };
 
-
+    
+    float minx = 10000000;
+    float miny = 10000000;
+    float minz = 10000000;
+    float maxx = -1000000;
+    float maxy = -1000000;
+    float maxz = -1000000;
   BBox bbox;
-
+  int objects = 0;
+  Vector3D avgCentroid;
   for (auto p = start; p != end; p++) {
     BBox bb = (*p)->get_bbox();
     bbox.expand(bb);
+      objects++;
+      Vector3D center = bb.centroid();
+      avgCentroid = avgCentroid + center;
+      minx = ( center.x < minx ? center.x: minx );
+      miny = ( center.y < miny ? center.y: miny );
+      minz = ( center.z < minz ? center.z: minz );
+      maxx = ( center.x > maxx ? center.x: maxx );
+      maxy = ( center.y > maxy ? center.y: maxy );
+      maxz = ( center.z > maxz ? center.z: maxz );
   }
-
+    int axis;
+    if(maxx-minx > maxy-miny && maxx-minx > maxz-minz){
+        axis = 0;
+        std::sort(start, end, less_than_x());
+    }
+    if(maxy-miny > maxx-minx && maxy-miny > maxz-minz){
+        axis = 1;
+        std::sort(start, end, less_than_y());
+    }
+    if(maxz-minz > maxy-miny && maxz-minz > maxx-minx){
+        axis = 2;
+        std::sort(start, end, less_than_z());
+    }
+  avgCentroid = avgCentroid * 1/objects;
   BVHNode *node = new BVHNode(bbox);
-  node->start = start;
-  node->end = end;
+    if(objects<max_leaf_size){
+        node->l = NULL;
+        node->r = NULL;
+        node->start = start;
+        node->end = end;
+    }
+    else{
+        std::vector<Primitive *>::iterator lstart = start;
+        std::vector<Primitive *>::iterator rend = end;
+        std::vector<Primitive *>::iterator lend;
+        std::vector<Primitive *>::iterator rstart;
+        float maxAxis = -111111111.1;
+        float minAxis = 111111111.1;
+        float centerAxis;
+        auto q = start;
+        while((*q)->get_bbox().centroid()[axis]<avgCentroid[axis]){
+            lend = q;
+            q++;
+            }
+        
+        if(lstart!=lend && lend !=rend){
+            node->l = construct_bvh(lstart,lend,max_leaf_size);
+            node->r = construct_bvh(lend,rend,max_leaf_size);}
+        else{
+            node->l = NULL;
+            node->r = NULL;
+            node->start = start;
+            node->end = end;
+        }
+        
+    }
 
   return node;
 
@@ -98,14 +178,30 @@ bool BVHAccel::intersect(const Ray &ray, Intersection *i, BVHNode *node) const {
   // TODO (Part 2.3):
   // Fill in the intersect function.
 
+    if(node->bb.intersect(ray, ray.min_t, ray.max_t)==false){
+        return false;
+    }
+    bool hit = false;
+    if(node->isLeaf()){
+        for (auto p=node->start; p!=node->end; p++) {
+          total_isects++;
+          hit = (*p)->intersect(ray, i) || hit;
+        }
+    }
+    else{
+        bool hit1 = intersect(ray, i, node->l);
+        bool hit2 = intersect(ray, i, node->r);
+        hit = hit1 || hit2;
 
-
-  bool hit = false;
-  for (auto p : primitives) {
-    total_isects++;
-    hit = p->intersect(ray, i) || hit;
-  }
+    }
   return hit;
+
+//  bool hit = false;
+//  for (auto p : primitives) {
+//    total_isects++;
+//    hit = p->intersect(ray, i) || hit;
+//  }
+//  return hit;
 
 
 }
